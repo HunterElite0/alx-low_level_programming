085295d5b1dc674cddb032e37a959839b9e2ee2e
HunterElite0 <faressskarim@gmail.com>
2023-03-16 10:22:25 -0700

    21038

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ *
+ * str - holds string
+ *
+ * puts - prints char array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return(0);
+	return (0);
 }
