97ab760b5cc8e780d0e1241a2f69a86a2f053faf
HunterElite0 <faressskarim@gmail.com>
2023-03-17 06:11:00 -0700

    17760

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Code starts here
+ *
+ *
+ * Return: 0 - Always true
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);	
+	putchar('\n');
+	return (0);
+}
