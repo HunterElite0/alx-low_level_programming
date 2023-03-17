27af5382574d0e2ebf518ba7b96ae729f97a7948
HunterElite0 <faressskarim@gmail.com>
2023-03-17 05:56:40 -0700

    21796

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Code starts here
+ *
+ *
+ * Return: 0 - Always true
+ */
+int main(void)
+{
+	char i = 'a';
+
+	for (; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
