ae5f8dace23a92d32f62259f2c756a6f26050426
HunterElite0 <faressskarim@gmail.com>
2023-03-17 07:37:16 -0700

    32578

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Code starts here
+ *
+ *
+ * Return: 0 - Always true
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int s;
+
+	for (i = 48 ; i <= '9'; i++)
+	{
+		for (j = i+1 ; j <= '9'; j++)
+		{
+			for(s = j+1 ; s <= '9' ; s++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(s);
+				if (i == '7' && j == '8' && s == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
