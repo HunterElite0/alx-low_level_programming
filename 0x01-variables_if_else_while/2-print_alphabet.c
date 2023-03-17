6028d6a9ffc75388601782f2473879058ea45227
HunterElite0 <faressskarim@gmail.com>
2023-03-17 05:50:26 -0700

    21965

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Code starts here
+ *
+ *
+ * Return: 0 - Always true
+ */
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+    	putchar('\n');
+}
