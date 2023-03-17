baee5d173655664ab1cb260808baf74b9584242f
HunterElite0 <faressskarim@gmail.com>
2023-03-17 06:01:12 -0700

    12344

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Code starts here
+ *
+ *
+ * Return: 0 - Always true
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
