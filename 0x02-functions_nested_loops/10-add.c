bb0f81c981d9cfcc58a820e443a1ad20b183833c
HunterElite0 <faressskarim@gmail.com>
2023-03-21 13:15:41 -0700

    18086

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *add - lol
+ *
+ *@n : int
+ *
+ *@p : int
+ *
+ *Return: 0 or 1
+ *
+*/
+int print_last_digit(int n, int p)
+{
+	return (n + p);
+}
