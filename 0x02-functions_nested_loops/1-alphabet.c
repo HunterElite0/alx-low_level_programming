30ef4c4cc73c97ea6696d17ce37fe90889c9857a
HunterElite0 <faressskarim@gmail.com>
2023-03-21 10:38:23 -0700

    3267

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,13 @@
 #include "main.h"
 
 /**
-*main - Entry point
-*
-*Return: Always 0 (Success)
+ *print_alphabet - prints alphabet
+ *
+ *Return: alphabet
+ *
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
@@ -15,6 +19,7 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 }
+
 int main(void)
 {
 	print_alphabet();
