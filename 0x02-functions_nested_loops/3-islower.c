701a8651c84fa76c752191b2018b1010bc79fd3f
HunterElite0 <faressskarim@gmail.com>
2023-03-21 10:52:20 -0700

    14432

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,14 @@
 /**
  *_islower - lol
  *
+ *@c - int
+ *
  *Return: alphabet
  *
 */
 int _islower(int c)
 {
-	if (c <= 97 && c <= 122)
-		return 1;
-	return 0;
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
 }
