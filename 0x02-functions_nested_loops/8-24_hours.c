75e67542e55549fd6c9564e5f80d36f9fefdaabe
HunterElite0 <faressskarim@gmail.com>
2023-03-21 11:42:03 -0700

    24706

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_last_digit - lol
+ *
+ *@n : int
+ *
+ *Return: 0 or 1
+ *
+*/
+void print_alphabet(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			printf("%d:%d\n",i,j);
+		}
+	}
+}
