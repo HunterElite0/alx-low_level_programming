f66bd3a0f41ac34913db733e24d8d0e9f6ed46c6
HunterElite0 <faressskarim@gmail.com>
2023-03-21 10:31:54 -0700

    1590

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i < 'z'; i++)
+	{
+		putchar(i);
+	}
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
