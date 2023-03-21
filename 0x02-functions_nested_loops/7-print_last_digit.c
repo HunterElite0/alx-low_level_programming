beb40939d4ec9f533af09b3bf397987f774f52d5
HunterElite0 <faressskarim@gmail.com>
2023-03-21 11:09:55 -0700

    24243

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_last_digit - lol
+ *
+ *@n : int
+ *
+ *Return: 0 or 1
+ *
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
