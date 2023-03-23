abb54c591bb01202e61c440bd349186b2f0b994c
HunterElite0 <faressskarim@gmail.com>
2023-03-23 13:23:09 -0700

    8300

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= i; j++)
 			{
@@ -23,5 +23,9 @@ void print_diagonal(int n)
 			_putchar('\\');
 		}
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(\n);
 	}
 }
