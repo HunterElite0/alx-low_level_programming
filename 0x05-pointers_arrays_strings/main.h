39866e9381395b422ee48f6edcd21929ea5b087d
HunterElite0 <faressskarim@gmail.com>
2023-03-27 07:31:44 -0700

    10882

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int n);
+void swap_int(inta, int b);
+int _strlen(chars);
+void _puts(char str);
+int _putchar(char c);
+void print_rev(chars);
+void rev_string(char s);
+void puts2(charstr);
+void puts_half(char str);
+void print_array(inta, int n);
+char _strcpy(chardest, char src);
+int _atoi(chars);
+
+#endif
