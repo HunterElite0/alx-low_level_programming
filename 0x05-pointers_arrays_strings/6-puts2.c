53c1c4f2acd209c5f673d2a34641029e37170397
HunterElite0 <faressskarim@gmail.com>
2023-03-27 09:15:26 -0700

    31460

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *puts2 - reverse string
+ *
+ *@str : string
+ *
+ *Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	putchar(s[0]);
+	for (i = 2; s[i] != '\0'; i += 2)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
