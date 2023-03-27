aeed4bd6fe47077cbfd34af7b08d6245255ba9d3
HunterElite0 <faressskarim@gmail.com>
2023-03-27 08:42:14 -0700

    23927

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_rev - reverse string
+ *
+ *@s : string
+ *
+ *Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int size = _strlen(s);
+
+	for (i = size-1; s[i] != '\0'; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
