cf95adea2ba1ae90c33085ad602555decc5eaed2
HunterElite0 <faressskarim@gmail.com>
2023-03-29 11:55:05 -0700

    24550

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ *string_toupper - uppercase
+ *
+ *@a : string
+ *
+ *Return: string
+*/
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		a[i] = toupper(a[i]);
+	}
+	return(a);
+}
