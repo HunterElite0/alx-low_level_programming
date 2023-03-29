5d9ef726bbb3613f0e104e73c3862a3f21bc9e85
HunterElite0 <faressskarim@gmail.com>
2023-03-29 10:43:24 -0700

    7516

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 	int size = strlen(dest);
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i + size] = src[i];
 		i++;
 	}
-	
+
 	return (dest);
 }
