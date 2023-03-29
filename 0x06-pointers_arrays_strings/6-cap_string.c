c7a9355129bdc021802e949a5879d8d3ae98e9eb
HunterElite0 <faressskarim@gmail.com>
2023-03-29 13:33:10 -0700

    30038

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ *cap_string - uppercase
+ *
+ *@a : string
+ *
+ *Return: string
+*/
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i - 1] == ' ')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '\t')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '\n')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == ',')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == ';')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '.')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '!')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '?')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '"')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '(')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == ')')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '{')
+			a[i] = toupper(a[i]);
+		if (a[i - 1] == '}')
+			a[i] = toupper(a[i]);
+	}
+
+	return (a);
+}
