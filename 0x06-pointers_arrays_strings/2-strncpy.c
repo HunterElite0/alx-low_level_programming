8763e19ec948dde726b1fba4d6bd8f00f0231bd1
HunterElite0 <faressskarim@gmail.com>
2023-03-29 10:58:13 -0700

    21843

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	if (n <= 98)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
