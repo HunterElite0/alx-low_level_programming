c8f2eb28e190d9e41818b41237c776508b7045e9
HunterElite0 <faressskarim@gmail.com>
2023-03-29 13:38:59 -0700

    28458

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,36 +13,40 @@
 char *cap_string(char *a)
 {
 	int i;
+	int size = strlen(a);
 
-	for (i = 0; a[i] != '\0'; i++)
+	if (size > 0)
 	{
-		if (a[i - 1] == ' ')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '\t')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '\n')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == ',')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == ';')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '.')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '!')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '?')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '"')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '(')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == ')')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '{')
-			a[i] = toupper(a[i]);
-		else if (a[i - 1] == '}')
-			a[i] = toupper(a[i]);
+		a[0] = toupper(a[0]);
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (a[i - 1] == ' ')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '\t')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '\n')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == ',')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == ';')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '.')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '!')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '?')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '"')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '(')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == ')')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '{')
+				a[i] = toupper(a[i]);
+			else if (a[i - 1] == '}')
+				a[i] = toupper(a[i]);
+		}
 	}
-
 	return (a);
 }
