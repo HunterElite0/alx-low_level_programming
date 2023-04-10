c110693f73f41b0a6c4d99e531a90164800c8a0f
HunterElite0 <faressskarim@gmail.com>
2023-04-10 09:01:24 -0700

    13223

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: arguement count
+ * @argv: arguments
+ *
+ * Return: nothing.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
