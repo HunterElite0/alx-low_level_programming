c25a48b63d0a06fdd868c59cd8b8170e619c0b5c
HunterElite0 <faressskarim@gmail.com>
2023-04-10 08:48:33 -0700

    13951

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+}
