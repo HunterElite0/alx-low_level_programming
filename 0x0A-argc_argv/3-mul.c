19fd0e9f839ea379e4140af01d502e8046fc13c6
HunterElite0 <faressskarim@gmail.com>
2023-04-10 09:14:57 -0700

    27803

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: arguement count
+ * @argv: arguments
+ *
+ * Return: nothing.
+ */
+int main(int argc, char **argv)
+{
+	if (argc - 1 < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+
+	return (0);
+}
