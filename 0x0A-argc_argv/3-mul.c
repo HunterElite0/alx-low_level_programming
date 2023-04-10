7d138a5626257fa7fd14282e3260e100eecf6827
HunterElite0 <faressskarim@gmail.com>
2023-04-10 09:18:25 -0700

    18888

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,7 @@ int main(int argc, char **argv)
 {
 	if (argc - 1 < 2)
 	{
-		(void)argv;
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
