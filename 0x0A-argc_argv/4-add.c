0c898444b04be5a1306d1cf2391971ca49d618d2
HunterElite0 <faressskarim@gmail.com>
2023-04-10 09:44:20 -0700

    24106

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - check the code
- * @argc: arguement count
- * @argv: arguments
+ * is_number - checks if the string passed is all digits
+ * @str: string to check
  *
- * Return: nothing.
+ * Return: 0 if fail, 1 if success
+ */
+int is_number(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str > 57 || *str < 48)
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: number of args passed
+ * @argv: array of args passed
+ *
+ * Return: 0 if success, 1 if passed parameters are not +ve numbers
  */
 int main(int argc, char **argv)
 {
-	int i;
-	int result = 0;
+	int count, result;
 
-	if (argc >= 3)
-	{
-		for (i = 1; i < argc; i++)
+	result = 0;
+	if (argc > 1)
+		for (count = 1; count < argc; count++)
 		{
-			printf("%d\n", atoi(argv[i]));
-			if (atoi(argv[i]) != 0)
-			{
-				result += atoi(argv[i]);
-			}
+			if (is_number(argv[count]) == 1)
+				result += atoi(argv[count]);
 			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-	}
-
 	printf("%d\n", result);
-
 	return (0);
 }
