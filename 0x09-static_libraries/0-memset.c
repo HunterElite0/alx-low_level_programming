63aa110a94c48d33f8591a1119e74957d3f21e8d
HunterElite0 <faressskarim@gmail.com>
2023-04-10 08:13:16 -0700

    14622

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memset - lol
+ *
+ *@b : char
+ *@s : string
+ *@n : int
+ *
+ *Return: 0 or 1
+ *
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
