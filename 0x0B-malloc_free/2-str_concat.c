b638d4430a4251d0e37f69008a584c9b5e0d9075
HunterElite0 <faressskarim@gmail.com>
2023-04-11 18:55:36 -0700

    30575

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,34 +13,40 @@ char *str_concat(char *s1, char *s2)
 {
 	int size1 = 0, size2 = 0;
 	int i;
-	char *pstr;
+	char *pstr = NULL;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 != NULL)
 	{
-		return (NULL);
+		for (i = 0; '\0' != s1[i]; i++)
+		{
+			size1++;
+		}
 	}
-	for (i = 0; '\0' != s1[i]; i++)
+	if (s2 != NULL)
 	{
-		size1++;
+		for (i = 0; '\0' != s2[i]; i++)
+		{
+			size2++;
+		}
 	}
-	for (i = 0; '\0' != s2[i]; i++)
-	{
-		size2++;
-	}
-	pstr = (char *) malloc(sizeof(char) * (size1 + size2));
+	pstr = (char *)malloc(sizeof(char) * (size1 + size2));
 	if (pstr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= size1; i++)
+	if (s1 != NULL)
 	{
-		pstr[i] = s1[i];
+		for (i = 0; i <= size1; i++)
+		{
+			pstr[i] = s1[i];
+		}
 	}
-	for (i = 0; i <= size2; i++)
+	if (s2 != NULL)
 	{
-		pstr[i+size1] = s2[i];
+		for (i = 0; i <= size2; i++)
+		{
+			pstr[i + size1] = s2[i];
+		}
 	}
-	
-	
 	return (pstr);
 }
