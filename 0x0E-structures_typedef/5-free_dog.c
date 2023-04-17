ee23c003e1973e359baa37ff416813f116ca204b
HunterElite0 <faressskarim@gmail.com>
2023-04-17 07:23:14 -0700

    22056

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ *
+ * @d: struct dog
+ */
+void *free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner;
+		free(d);
+	}
+}
