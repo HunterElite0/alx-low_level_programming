e059636a78f0cbee7895dd227487bb50787466a0
HunterElite0 <faressskarim@gmail.com>
2023-04-17 06:52:18 -0700

    25165

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initializes dog struct
+ *
+ * @d: struct dog
+ * @name: string
+ * @age: int
+ * @owner: string
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
