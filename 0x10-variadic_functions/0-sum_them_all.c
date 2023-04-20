cf12fd491c3b8b38cd9937c41814511cdcc17640
HunterElite0 <faressskarim@gmail.com>
2023-04-20 06:59:05 -0700

    6944

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all numbers
+ *
+ * @n: int
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	int result = 0;
+	va_list v;
+	int i = 0;
+
+	va_start(v, n);
+	for (i = 0; i < n; i++)
+	{
+		result += va_arg(v, int);
+	}
+	va_end(v);
+	return (result);
+}
