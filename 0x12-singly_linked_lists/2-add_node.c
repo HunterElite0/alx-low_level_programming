123103237d6d19a71b76cc7933f4be9b1989d6f1
HunterElite0 <faressskarim@gmail.com>
2023-04-27 17:12:34 -0700

    11219

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - adds node
+ * @head: head
+ * @str: string
+ * Return: ptr to head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	int l = 0;
+
+	while (str[l] != '\0')
+		l++;
+
+	node = malloc(sizeof(list_t));
+	node->next = *head;
+	node->str = strdup(str);
+	node->len = l;
+	*head = node;
+
+	return (*head);
+}
